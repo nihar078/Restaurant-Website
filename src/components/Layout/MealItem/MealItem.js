cb12aa8438f0afbcfb7eb3cb230df604618988c3
nihar078 <nihar078@gmail.com>
2023-07-06 13:04:59 +0530

    Adding form

--- src/components/Layout/MealItem/MealItem.js
+++ src/components/Layout/MealItem/MealItem.js
@@ -1,5 +1,6 @@
-
+import React from 'react'
 import classes from './MealItem.module.css'
+import MealItemForm from './MealItemForm'
 
 
 const MealItem = (props) =>{
@@ -10,6 +11,9 @@ const MealItem = (props) =>{
                 <h3>{props.name}</h3>
                 <div className={classes.description}>{props.description}</div>
                 <div className={classes.price}>${price}</div>
+            </div>
+            <div>
+                <MealItemForm />
             </div>
         </li>
     )
