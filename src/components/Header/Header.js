0058502e3f77a83d5d6289b590b0f9db14606292
nihar078 <nihar078@gmail.com>
2023-07-06 11:00:28 +0530

    add Header of Restaurant

---
+++ src/components/Header/Header.js
@@ -0,0 +1,21 @@
+import React, { Fragment } from "react";
+import mealsImage from "../../assets/meals.jpg"
+import classes from './Header.module.css'
+
+
+const Header = () => {
+    return (
+        <Fragment>
+            <header className={classes.header}>
+                <h1>ReactMelas</h1>
+                <button>Cart</button>
+            </header>
+            <div className={classes["main-image"]}>
+                <img src={mealsImage} alt="A table full of delicious food!"></img>
+            </div>
+
+        </Fragment>
+    )
+}
+
+export default Header
